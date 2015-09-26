edf858c52753be072ccc72649c9732d07fff568a
hunghuynh <huynhphihung1990@gmail.com>
2015-09-26 23:19:06 +0700

    Init design and create view controller for app

---
+++ QAchat/ViewController/AnswerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnswerViewController.h
+//  QAchat
+//
+//  Created by Keng on 9/26/15.
+//  Copyright © 2015 HungHuynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerViewController : UIViewController
+
+@end
