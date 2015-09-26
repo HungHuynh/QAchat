f264afb01714d5d823d68b6bcd0f3fe4f4a0f978
hunghuynh <huynhphihung1990@gmail.com>
2015-09-26 10:45:04 +0700

    init project

---
+++ QAchat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QAchat
+//
+//  Created by Keng on 9/26/15.
+//  Copyright © 2015 HungHuynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
